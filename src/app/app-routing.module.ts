8bf264c2553d63f07cc56b62ced596ba25d0e590
Batul <albatularts@gmail.com>
2024-07-15 16:50:44 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './main-apps/cart/cart.component';
+import { HomeComponent } from './main-apps/home/home.component';
+import { AccountComponent } from './main-apps/account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'cart',
+    component:CartComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+
+  },
+  {
+    path:'account',
+    component:AccountComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CartComponent } from './main-apps/cart/cart.component';
+import { HomeComponent } from './main-apps/home/home.component';
+import { AccountComponent } from './main-apps/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    HomeComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
