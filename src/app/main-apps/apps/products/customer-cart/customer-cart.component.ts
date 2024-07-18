2d9773f4b1482ebbf2760235e19f1531cf53247c
Batul <albatularts@gmail.com>
2024-07-18 23:12:09 +0300

    done

--- src/app/main-apps/apps/products/customer-cart/customer-cart.component.ts
+++ src/app/main-apps/apps/products/customer-cart/customer-cart.component.ts
@@ -13,26 +13,36 @@ export class CustomerCartComponent implements OnInit {
       title: ' شوفان كويكر 500جم',
       image: '../../../../../assets/images/Oats.jpg',
       info: 15,
-      barcode: '../../../../../assets/images/Barcode.png'
+      barcode: '../../../../../assets/images/Barcode.png',
+      amount: 1
     },
     {
       title: ' شوفان كويكر 500جم',
       image: '../../../../../assets/images/Oats.jpg',
       info: 15,
-      barcode: '../../../../../assets/images/Barcode.png'
+      barcode: '../../../../../assets/images/Barcode.png',
+      amount: 1
+
     },
     {
       title: ' شوفان كويكر 500جم',
       image: '../../../../../assets/images/Oats.jpg',
       info: 15,
-      barcode: '../../../../../assets/images/Barcode.png'
+      barcode: '../../../../../assets/images/Barcode.png',
+      amount: 1
     }
   ]
 
   cards = [1345, 8763, 7398]
-  amount = 1
+
   count = this.products.length
   shopname = "متجر بنده | فرع الشرائع "
+  shopimage = "../../../../../assets/images/Panda.png"
+  doneOrder = "ادفع الآن"
+  doneOrderLink = "link"
+  location = "الشرائع, الفردوس 3726 (بيت)"
+  delivery = 20
+  wallet = 0
 
   getPrice() {
     let val = 0;
@@ -45,14 +55,26 @@ export class CustomerCartComponent implements OnInit {
 
   price = this.getPrice()
 
+  total = this.price + this.delivery - this.wallet
+
+
+
   //@ViewChild('child') child: CustomerMyInformationComponent
-  delivery = 20
-  wallet = 0
+
   //wallet = this.getWallet();
   //  getWallet() {
   //    return this.child.wallet
   // }
-  total = this.price + this.delivery - this.wallet
+
+  inc(index) {
+    index.amount += 1;
+  }
+
+  dec(index) {
+    if (index.amount > 0) {
+      index.amount -= 1;
+    }
+  }
 
   constructor() { }
 
