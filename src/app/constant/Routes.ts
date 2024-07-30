de8468c25cb7ff6bda24bf39ef1a1d375b1f3e48
Fouad Mohammed Adam <faadoon@gmail.com>
2024-07-30 17:01:51 +0300

    last

--- src/app/constant/Routes.ts
+++ src/app/constant/Routes.ts
@@ -8,22 +8,19 @@ export const auth = {
   register: BASEURL + 'register',
   verifyOtp: BASEURL + 'verifyOtp',
   logout: BASEURL + 'logout',
-
 }
 
 export const HOME = {
+//  total_all_users_manager: BASEURL + 'getNumberOfUsers',
  total_users_customers_manager: BASEURL + 'getNumberOfUsers',
+ total_users_collectors_manager: BASEURL + 'getNumberOfCollectors',
+ total_users_markets_manager: BASEURL + 'getNumberOfMarkets',
  list_users_customers: BASEURL + 'showAllUsersDetailedInfo',
  list_users_collectors: BASEURL + 'showAllCollectorsDetailedInfo',
+ list_users_markets: BASEURL + 'showAllMarketsDetailedInfo',
 }
-
-
-
 
 export const collector = {
   collectorUpdate: BASEURL + 'collectorUpdate',
-
-
-
 }
 
